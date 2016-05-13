aeb739333ea5ca6701b479e0870f788b6f953025
hendar <hendar.133040294@mail.unpas.ac.id>
2016-05-13 16:49:13 +0700

    second commit

---
+++ src/main/java/com/rkppl/ujian/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rkppl.ujian;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+        
+        logger.error("ini adalah debug");
+        logger.info("ini adalah info message");   
+        
+    }
+}
